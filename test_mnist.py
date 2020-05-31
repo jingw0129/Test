526d5acb7bb94148b91c22bbe4ba3b7df1ca9337
Aimee Wang <22219702+jingw0129@users.noreply.github.com>
2020-05-31 14:30:41 +0800

    Add files via upload

--- test_mnist.py
+++ test_mnist.py
@@ -4,28 +4,7 @@ from mnist_net import Net
 from torchvision import datasets, transforms
 import cv2
 
-model_path = 'mnist_model/18model.pth'
-
-def test():
-  correct_count, all_count = 0, 0
-  for images, labels in valloader:
-      for i in range(len(labels)):
-          img = images[i].view(1, 784)
-          with torch.no_grad():
-              logps = net(img)
-
-          ps = torch.exp(logps)
-          probab = list(ps.numpy()[0])
-          pred_label = probab.index(max(probab))
-          true_label = labels.numpy()[i]
-          if (true_label == pred_label):
-              correct_count += 1
-          all_count += 1
-
-  print("Number Of Images Tested =", all_count)
-  print("\nModel Accuracy =", (correct_count / all_count))
-
-# test()
+model_path = 'mnist_model/27model.pth'
 
 
 # -----------------------------------------------------------------------------------------
